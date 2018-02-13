09a89411494f6f21b3583377d9fa59b2da95abf8
Jarod Legault <jarod.legault@gmail.com>
2018-02-13 19:05:41 +0000

    add mongolab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://jarodl:yelpcamp@ds025419.mlab.com:25419/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
