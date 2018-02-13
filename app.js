9e8bdb6e5330f4ebe23c95b831691786d7832dd6
Jarod Legault <jarod.legault@gmail.com>
2018-02-13 20:36:39 +0000

    change db to env. variable

--- app.js
+++ app.js
@@ -16,8 +16,10 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
 
-//mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://jarodl:yelpcamp@ds025419.mlab.com:25419/yelpcamp");
+mongoose.connect(process.env.DATABASEURL);
+//mongoose.connect("mongodb://localhost/yelp_camp"); // Dev
+//mongoose.connect("mongodb://jarodl:yelpcamp@ds025419.mlab.com:25419/yelpcamp"); // Prod
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
